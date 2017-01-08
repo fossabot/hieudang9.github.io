2e4f27953847d0105e565059978535a6ab789c1c
Hieu Dang <hieu@hieudang.com>
2017-01-08 14:21:35 +0700

    Add analytics and modify post

---
+++ assets/js/pi.js
@@ -0,0 +1,10 @@
+  var _paq = _paq || [];
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="//pi.sufy.us/";
+    _paq.push(['setTrackerUrl', u+'pi.php']);
+    _paq.push(['setSiteId', '4']);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'pi.js'; s.parentNode.insertBefore(g,s);
+  })();
